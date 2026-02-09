0e6be2392f9d5d44be24371101af6eedc92c28db
jalanwang <jalanwang@gmail.com>
2026-02-09 14:19:22 +0900

    Add DistTurtle action server implementation; include parameter handling and feedback mechanism

---
+++ src/my_move_turtle_pkg/my_move_turtle_pkg/dist_turtle_action_server_gui_log2.py
@@ -0,0 +1,125 @@
+#dist_turtle_action_server_gui_log1.py
+#기존 파일이 있어서 부득이 이름을 변경했다.
+#파라미터값을 초기에 출력하는 루틴이 추가되었다.
+#파라미터값이 수정되면, 그 값을 출력하는 루틴이 추가되었다.
+
+import rclpy as rp
+from rclpy.action import ActionServer
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.node import Node
+
+from turtlesim.msg import Pose
+from geometry_msgs.msg import Twist
+from my_first_package_msgs.action import DistTurtle
+from my_move_turtle_pkg.turtlesim_subscriber import TurtlesimSubscriber
+
+from rcl_interfaces.msg import SetParametersResult
+# 파라미터 결과값이 바뀌면 처리하기 위한 임포트
+
+import math
+import time
+
+class TurtleSub_Action(TurtlesimSubscriber):
+    def __init__(self, ac_server):
+        super().__init__()
+        self.ac_server = ac_server
+
+    def callback(self, msg):
+        self.ac_server.current_pose = msg
+
+class DistTurtleServer(Node):
+    def __init__(self):
+        super().__init__('dist_turtle_action_server')
+        self.total_dist = 0.0
+        self.is_first_time = True
+        self.current_pose = Pose()
+        self.previous_pose = Pose()
+        self.publisher = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        self.action_server = ActionServer(self, DistTurtle, 'dist_turtle', self.execute_callback)
+
+        self.get_logger().info('Dist turtle action server is started.')
+        self.declare_parameter('quantile_time', 0.75)
+        self.declare_parameter('almost_goal_time', 0.95)
+
+        # 파라메타를 받아와서 프린트 해주는 부분
+        quantile_time, almost_goal_time = self.get_parameters(
+            ['quantile_time', 'almost_goal_time'])
+        print('quantile_time and almost_goal_time is', quantile_time.value, almost_goal_time.value)
+
+        # 파라미터가 변경되었을 때 호출되는 콜백 함수 등록
+        self.add_on_set_parameters_callback(self.parameter_callback)
+        self.get_logger().info('Parameter callback registered')
+
+    def parameter_callback(self, params):
+        self.get_logger().info(f'parameter_callback called with {len(params)} parameters')
+        for param in params:
+            print(param.name, " is changed to ", param.value)
+        return SetParametersResult(successful=True)
+
+    def calc_diff_pose(self):
+        if self.is_first_time:
+            self.previous_pose.x = self.current_pose.x
+            self.previous_pose.y = self.current_pose.y
+            self.is_first_time = False
+
+        diff_dist = math.sqrt((self.current_pose.x - self.previous_pose.x)**2 +\
+                             (self.current_pose.y - self.previous_pose.y)**2)
+
+        self.previous_pose = self.current_pose
+
+        return diff_dist
+
+    def execute_callback(self, goal_handle):
+        feedback_msg = DistTurtle.Feedback()
+
+        msg = Twist()
+        msg.linear.x = goal_handle.request.linear_x
+        msg.angular.z = goal_handle.request.angular_z
+
+        while True:
+            self.total_dist += self.calc_diff_pose()
+            feedback_msg.remained_dist = goal_handle.request.dist - self.total_dist
+            goal_handle.publish_feedback(feedback_msg)
+            self.publisher.publish(msg)
+            time.sleep(0.1)
+
+            if feedback_msg.remained_dist < 0.2:
+                break
+
+        goal_handle.succeed()
+        result = DistTurtle.Result()
+
+        result.pos_x = self.current_pose.x
+        result.pos_y = self.current_pose.y
+        result.pos_theta = self.current_pose.theta
+        result.result_dist = self.total_dist
+
+        self.total_dist = 0
+        self.is_first_time = True
+
+        return result
+
+
+def main(args=None):
+    rp.init(args=args)
+
+    executor = MultiThreadedExecutor()
+
+    ac = DistTurtleServer()
+    sub = TurtleSub_Action(ac_server = ac)
+
+    executor.add_node(sub)
+    executor.add_node(ac)
+
+    try:
+        executor.spin()
+
+    finally:
+        executor.shutdown()
+        sub.destroy_node()
+        ac.destroy_node()
+        rp.shutdown()
+
+
+if __name__ == '__main__':
+    main()
